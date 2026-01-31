245b747e484176a6abdfc4a61c817b07539a0b98
EchinaceaPuff <tadpole41444@gmail.com>
2026-01-31 17:01:39 -0600

    Got the calendar to work

--- Front-End/src/Pages/Calendar.jsx
+++ Front-End/src/Pages/Calendar.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
-import './calendar.css'
+import './Calendar.css'
 
 
 
 function App() {
   const [date, setDate] = useState(new Date());
+  const [ myDate, onChange ] = useState(new Date());
+  const tileStuff = ({ date, view }) => {
+    if (view === 'month' && myDate && date.toDateString() === myDate.toDateString()) {
+      return 'someClass';
+    }
+  };
 
   return (
     <div className='app'>
-      <h1 className='text-center'>React Calendar</h1>
       <div className='calendar-container'>
-        
+        <Calendar 
+        onChange={onChange} 
+        value={date}
+        tileClassName={tileStuff}
+        />
         
       </div>
     
