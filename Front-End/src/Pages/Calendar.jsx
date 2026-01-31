6d4f7d3e28fe2443580dd09706ed9260c8ce0e31
EchinaceaPuff <tadpole41444@gmail.com>
2026-01-31 14:32:30 -0600

    Working on the calendar

--- Front-End/src/Pages/Calendar.jsx
+++ Front-End/src/Pages/Calendar.jsx
@@ -1,3 +1,27 @@
-    export default function Calendar() {
-  return <h2>Calendar </h2>;
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import './calendar.css'
+
+
+
+function App() {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className='app'>
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+        
+        
+      </div>
+    
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
+
+    </div>
+  );
 }
+
+export default App;
