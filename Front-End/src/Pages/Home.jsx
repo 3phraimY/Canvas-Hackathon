0f1485e0104550f6c9828acb162216c26e669e7b
3phraimY <ephraim.young@cuw.edu>
2026-01-31 17:40:34 -0600

    fix fetching

--- Front-End/src/Pages/Home.jsx
+++ Front-End/src/Pages/Home.jsx
@@ -1,21 +1,23 @@
 import WeekCalendar from "../Components/WeekCalendar";
-import { useState, useEffect } from 'react';
-import {listCourses} from '../hooks/CanvasAPI';
+import { useState, useEffect } from "react";
+import { listCourses } from "../hooks/CanvasAPI";
 
 export default function Home() {
-
   const [coursedata, setcoursedata] = useState(0);
 
   useEffect(() => {
-    setcoursedata(listCourses);
-    console.log(coursedata);
+    async function fetchCourses() {
+      const data = await listCourses();
+      setcoursedata(data);
+    }
+    fetchCourses();
   }, []);
-
 
   return (
     <>
       <h2>Week At a Glance</h2>
-      <h3>Tomorrow</h3>
+      <h3>Tomorrow </h3>
+      <div>{coursedata}</div>
       <p>Class </p>
       <p>Class name; homework assignments</p>
       <h3>Next 2 Days</h3>
@@ -23,8 +25,6 @@ export default function Home() {
       <p>Class name; homework assignments</p>
       <h2>This Week</h2>
       <WeekCalendar />
-      
     </>
   );
 }
-
--- Front-End/src/hooks/CanvasAPI.jsx
+++ Front-End/src/hooks/CanvasAPI.jsx
@@ -1,12 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 export async function listCourses() {
-
-    try {
-        const response = await axios.get('http://localhost:3000/listCourses');
-        console.log(response);
-        return response;
-    } catch (error) {
-        console.error('Error fecting data:', error);
-        return error;
-    }
-};
+  try {
+    const response = await axios.get("http://localhost:3000/listCourses");
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error("Error fecting data:", error);
+    return error;
+  }
+}
