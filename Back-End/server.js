d49b7840681799c5f596bc2d699395343037ce5b
3phraimY <ephraim.young@cuw.edu>
2026-01-31 14:14:55 -0600

    finished calendar request

--- Back-End/server.js
+++ Back-End/server.js
@@ -11,7 +11,6 @@ const app = express();
 
 // Route definitions
 app.get("/listCourses", listCourses);
-app.get("/courseAssignments/:courseId", courseAssignments);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
 module.exports = app;
